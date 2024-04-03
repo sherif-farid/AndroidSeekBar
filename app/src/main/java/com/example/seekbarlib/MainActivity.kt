89b923eb233a30819197393c39017d9d963f8cbc
sherif <sherffareed39@gmail.com>
2024-04-04 00:01:41 +0200

    update ui

--- app/src/main/java/com/example/seekbarlib/MainActivity.kt
+++ app/src/main/java/com/example/seekbarlib/MainActivity.kt
@@ -16,43 +16,43 @@ class MainActivity : AppCompatActivity(), RangeBarGradiant.OnRangeChanged {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         val stepsList1 = arrayListOf<Int>()
-        for (i in 1..500) {
+        for (i in 1..20) {
             stepsList1.add(i *10)
         }
         val stepsList2 = arrayListOf<Int>()
         for (i in 1..5) {
             stepsList2.add(i *10)
         }
-        binding.bar.setStepsList(stepsList1 , 24)
+        binding.bar.setStepsList(stepsList1 , 10)
         //uncomment to set a specific step
         //binding.bar.setCurrentStep(3)
         binding.bar.onRangeChanged = this
         binding.btn1.setOnClickListener {
-            binding.bar.setStepsList(stepsList1 , 24)
+            binding.bar.setStepsList(stepsList1 , 15)
             binding.bar.setCurrentStep(0)
             binding.min.text = stepsList1.first().toString()
-            binding.recommendedTxt.text = "${stepsList1[24]}"
+            binding.recTxt.text = "${stepsList1[15]}"
             binding.max.text = stepsList1.last().toString()
         }
         binding.btn2.setOnClickListener {
-            binding.bar.setStepsList(stepsList1 , 24)
+            binding.bar.setStepsList(stepsList1 , 8)
             binding.bar.setCurrentStep(stepsList1.size -1)
             binding.min.text = stepsList1.first().toString()
-            binding.recommendedTxt.text = "${stepsList1[24]}"
+            binding.recTxt.text = "${stepsList1[8]}"
             binding.max.text = stepsList1.last().toString()
         }
         binding.btn3.setOnClickListener {
             binding.bar.setStepsList(stepsList2 , 3)
             binding.bar.setCurrentStep(2)
             binding.min.text = stepsList2.first().toString()
-            binding.recommendedTxt.text = "${stepsList2[3]}"
+            binding.recTxt.text = "${stepsList2[3]}"
             binding.max.text = stepsList2.last().toString()
         }
         binding.btn4.setOnClickListener {
             binding.bar.setStepsList(stepsList2 , 2)
             binding.bar.setCurrentStep(4)
             binding.min.text = stepsList2.first().toString()
-            binding.recommendedTxt.text = "${stepsList2[2]}"
+            binding.recTxt.text = "${stepsList2[2]}"
             binding.max.text = stepsList2.last().toString()
         }
     }
